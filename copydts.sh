7ae19aed176407ec0df627e51094bb5b96f8e061
rudy <rs@anb-rimex.be>
2018-04-20 17:09:28 +0200

    maj

--- copydts.sh
+++ copydts.sh
@@ -1,5 +1,5 @@
-#scp ./output/*.dtb root@192.168.0.107:/media/boot
-scp ./output/*.dtb root@192.168.0.154:/media/boot
+scp ./output/*.dtb root@192.168.0.107:/media/boot
+#scp ./output/*.dtb root@192.168.0.154:/media/boot
 
 
 
