c7cace521be068f7da6bfbb68f01ab6e343cad6f
rudy <rs@anb-rimex.be>
2018-04-24 19:05:41 +0200

    restored sh files for ssh 192.168.0.154

--- copyzimage.sh
+++ copyzimage.sh
@@ -1,2 +1,2 @@
-scp ./output/zImage root@192.168.0.107:/media/boot/zImage
-#scp ./output/zImage root@192.168.0.154:/media/boot/zImage
+#scp ./output/zImage root@192.168.0.107:/media/boot/zImage
+scp ./output/zImage root@192.168.0.154:/media/boot/zImage
