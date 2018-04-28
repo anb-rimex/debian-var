bee0de9e43d9b00e7508fbf7adf32726b9f42b99
rudy <rs@anb-rimex.be>
2018-04-28 17:54:20 +0200

    to save sd contents

---
+++ copy_sd_to_pc.sh
@@ -0,0 +1,5 @@
+sudo rsync -axHAX --progress /media/rudy/BOOT-VARSOM/ /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/BOOT-VARSOM/
+sync
+sudo rsync -axHAX --progress /media/rudy/rootfs/ /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/
+sync
+
