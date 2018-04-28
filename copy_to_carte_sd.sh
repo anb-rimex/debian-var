625994f7a890804a801d340ac8e6eb34906b34d4
rudy <rs@anb-rimex.be>
2018-04-28 17:51:09 +0200

    ignoring sd directory maj transfert sd

--- copy_to_carte_sd.sh
+++ copy_to_carte_sd.sh
@@ -1,39 +1,39 @@
 sync
 sudo rm -rf /media/rudy/rootfs/*
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/etc/ /media/rudy/rootfs/etc/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/etc/ /media/rudy/rootfs/etc/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/proc/ /media/rudy/rootfs/proc/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/proc/ /media/rudy/rootfs/proc/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/sys/ /media/rudy/rootfs/sys/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/sys/ /media/rudy/rootfs/sys/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/dev/ /media/rudy/rootfs/dev/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/dev/ /media/rudy/rootfs/dev/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/mnt/ /media/rudy/rootfs/mnt/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/mnt/ /media/rudy/rootfs/mnt/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/srv/ /media/rudy/rootfs/srv/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/srv/ /media/rudy/rootfs/srv/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/usr/ /media/rudy/rootfs/usr/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/usr/ /media/rudy/rootfs/usr/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/var/ /media/rudy/rootfs/var/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/var/ /media/rudy/rootfs/var/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/run/ /media/rudy/rootfs/run/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/run/ /media/rudy/rootfs/run/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/sbin/ /media/rudy/rootfs/sbin/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/sbin/ /media/rudy/rootfs/sbin/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/bin/ /media/rudy/rootfs/bin/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/bin/ /media/rudy/rootfs/bin/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/lib/ /media/rudy/rootfs/lib/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/lib/ /media/rudy/rootfs/lib/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/opt/ /media/rudy/rootfs/opt/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/opt/ /media/rudy/rootfs/opt/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/media/ /media/rudy/rootfs/media/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/media/ /media/rudy/rootfs/media/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/home/ /media/rudy/rootfs/home/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/home/ /media/rudy/rootfs/home/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/root/ /media/rudy/rootfs/root/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/root/ /media/rudy/rootfs/root/
 sync
-sudo rsync -axHAX --progress /home/rudy/var_mx6ul_dart_debian_strech_9_3/sd/rootfs/tmp/ /media/rudy/rootfs/tmp/
+sudo rsync -axHAX --progress --verbose /home/rudy/TricomV2/var_mx6ul_dart_debian/sd/rootfs/tmp/ /media/rudy/rootfs/tmp/
 sync
 
 
--- prepare_carte_sd.sh
+++ prepare_carte_sd.sh
@@ -1,4 +1,4 @@
-sudo /home/rudy/var_mx6ul_dart_debian_strech_9_3/make_var_mx6ul_dart_debian.sh --cmd sdcardwc --dev /dev/sdb
+sudo /home/rudy/TricomV2/var_mx6ul_dart_debian/make_var_mx6ul_dart_debian.sh --cmd sdcardwc --dev /dev/sdb
 sync
 
 
