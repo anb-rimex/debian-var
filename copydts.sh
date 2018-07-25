91a9b491fbd8358c91960d92f02ca8c32279cdb9
rudy <rs@anb-rimex.be>
2018-07-25 11:26:33 +0200

    Modif rs

--- copydts.sh
+++ copydts.sh
@@ -1,6 +1,6 @@
 #scp ./output/*.dtb root@192.168.0.107:/media/boot
-scp ./output/*.dtb root@192.168.0.104:/media/boot
-#scp ./output/zImage root@192.168.0.154:/media/boot
+scp ./output/*.dtb root@192.168.0.129:/media/boot
+scp ./output/zImage root@192.168.0.129:/media/boot
 
 
 
